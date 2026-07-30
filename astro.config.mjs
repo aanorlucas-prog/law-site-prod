8cb77a6c61e709cc756f0d2404b43e38b13f1e52
Aanor <aanor.lucas@proton.me>
2026-07-30 16:37:31 +0200

    fix cloudflare pb

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,11 @@
 
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwind()],
   },
